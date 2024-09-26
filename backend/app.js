8e8fceb553e78637a74782c169e5226f873e1200
Rajan <rajanj448@student.sfit.ac.in>
2024-09-26 18:47:27 +0530

    middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { config } from "dotenv";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 
 
 const app=express();
@@ -7,6 +10,21 @@ const app=express();
 config({
     path: "./config/config.env",
   });
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+}))
 
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
+    })
+  );
 
 export default app;
